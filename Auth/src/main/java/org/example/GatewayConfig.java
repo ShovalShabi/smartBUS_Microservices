e66f1aa013ecabc05208b1eec4b523dde3369fab
tamir303 <tamir303@gmail.com>
2024-05-09 18:50:02 +0300

    created gateway for micro services

---
+++ Auth/src/main/java/org/example/GatewayConfig.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("route_service", r -> r
+                        .path("/route/**")
+                        .uri("http://localhost:10094/route"))
+                .build();
+    }
+}
