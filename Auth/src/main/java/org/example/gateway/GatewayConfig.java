7cdfd5c96ffb60983f5708858370d03d7ce02af0
tamir303 <tamir303@gmail.com>
2024-05-10 10:28:10 +0300

    first gateway test

--- Auth/src/main/java/org/example/gateway/GatewayConfig.java
+++ Auth/src/main/java/org/example/gateway/GatewayConfig.java
@@ -1,9 +1,18 @@
-package org.example;
+package org.example.gateway;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @Configuration
 public class GatewayConfig {
