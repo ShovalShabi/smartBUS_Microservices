abdadd2d9d5f707ca0c09307989e448109bf330f
ShovalShabi <shovalshabi@gmail.com>
2024-08-28 18:18:06 +0300

    Added configuration for the cross-origin of the service

--- OrderBus/src/main/java/org/example/api/WebClientConfiguration.java
+++ OrderBus/src/main/java/org/example/api/WebClientConfiguration.java
@@ -1,16 +1,38 @@
 package org.example.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static org.example.utils.Constants.ROUTES_SERVICE_URL;
 
 @Configuration
 public class WebClientConfiguration {
+
+    // Inject the client.orderbus property from application.properties or application.yml
+    @Value("${client.orderbus}")
+    private String orderBusUrl;
+
     @Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder()
                 .baseUrl(ROUTES_SERVICE_URL);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(orderBusUrl) // later on more clients will be added
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
