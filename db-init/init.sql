fd2d69487ff375d7e18a2d4b60a852dae1832f2a
ShovalShabi <shovalshabi@gmail.com>
2024-10-01 03:24:14 +0300

    Initialization script for the DBs for Feedback and Auth services, runs on postgres container startup

---
+++ db-init/init.sql
@@ -0,0 +1,7 @@
+-- Create databases for Auth
+CREATE DATABASE "AuthDBDev";
+CREATE DATABASE "AuthDBProd";
+
+-- Create databases for Feedback
+CREATE DATABASE "FeedbackDBDev";
+CREATE DATABASE "FeedbackDBProd";
